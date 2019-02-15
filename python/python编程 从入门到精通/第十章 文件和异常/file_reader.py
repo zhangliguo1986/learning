23696ee7a625f69f3533f5703c3c1e6cb752a97e
zhangliguo <zhangliguo@hckuku.com>
2019-02-15 10:25:56 +0800

    添加忽略文件

--- python/python编程 从入门到精通/第十章 文件和异常/file_reader.py
+++ python/python编程 从入门到精通/第十章 文件和异常/file_reader.py
@@ -1,5 +1,5 @@
 filename = 'pi_digits.txt'
 
-# with open(filename) as file_object:
-#     for line in file_object:
-#         print(line)
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
