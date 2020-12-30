fc0f24846bcb655e4ecfc903f233f2434c8d3b25
zhangliguo <zhangliguo@mingtian.com>
2020-12-30 14:31:24 +0800

    添加注释

--- C#/IoC/Program.cs
+++ C#/IoC/Program.cs
@@ -6,10 +6,13 @@ namespace IoC
     {
         static void Main(string[] args)
         {
+            // 创建容器实例
             var container = new Container();
+            // 注册服务
             container.RegisterTransient<IProductDAL, ProductDAL>();
             container.RegisterTransient<IProductBLL, ProductBLL>();
 
+            // 从容器中获取类型实例
             var productBLL = container.Resolve<IProductBLL>();
             var products = productBLL.GetProducts();
 
