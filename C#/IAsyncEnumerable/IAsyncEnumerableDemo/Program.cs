cbe08107692e1ef3081159013bd4154f962c15d1
zhangliguo <zhangliguo@mingtian.com>
2021-01-19 13:24:15 +0800

    添加IAsyncEnumberable 异步流使用方式返回数据demo

---
+++ C#/IAsyncEnumerable/IAsyncEnumerableDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IAsyncEnumerableDemo
+{
+    class Program
+    {
+        const int DELAY = 1000;
+        const int MIN = 1;
+        const int MAX = 10;
+
+        static async Task Main(string[] args)
+        {
+            await foreach (var number in GetData())
+            {
+                Console.WriteLine($"{DateTime.Now}: number={number}");
+            }
+            
+            Console.ReadKey();
+        }
+
+        static async IAsyncEnumerable<int> GetData()
+        {
+            for (int i = MIN; i < MAX; i++)
+            {
+                yield return i;
+                await Task.Delay(DELAY);
+            }
+        }
+    }
+}
